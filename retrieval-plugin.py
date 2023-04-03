27042f24702425e52177ddbaa0c0fd92b9797519
Johannes Messner <messnerjo@gmail.com>
2023-04-03 16:27:38 +0200

    feat: cloud deploy option

--- retrieval-plugin.py
+++ retrieval-plugin.py
@@ -1,7 +1,7 @@
 import os
+from argparse import Namespace
 from typing import Optional
 from jina import Flow
-from jina.excepts import BadImageNameError, RuntimeFailToStart
 from gateway import RetrievalGateway
 from datastore.executor.docarray_v1 import DocArrayDataStore
 
@@ -52,6 +52,30 @@ def launch(bearer_token: Optional[str], openai_token: Optional[str]):
         raise e
     with flow:
         flow.block()
+
+
+@app.command()
+def deploy(bearer_token: Optional[str], openai_token: Optional[str]):
+    from pathlib import Path
+    from jcloud.api import deploy as deploy_flow
+    import tempfile
+    from hubble.api import login as login_jina
+
+    args = Namespace(force=False)
+    login_jina(args)
+
+    config_str = Path('flow.yml').read_text()
+    config_str = config_str.replace('<your-openai-api-key>', openai_token)
+    config_str = config_str.replace('<your-bearer-token>', bearer_token)
+    tmp_config_file, tmp_config_path = tempfile.mkstemp()
+    try:
+        with os.fdopen(tmp_config_file, 'w') as tmp:
+            tmp.write(config_str)
+            args = Namespace(path=tmp_config_path)
+        deploy_flow(args)
+
+    finally:
+        os.remove(tmp_config_path)
 
 
 if __name__ == "__main__":
