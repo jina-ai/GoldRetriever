722ddcf43519c3c1598b346dfdaceffb68b3e012
jupyterjazz <saba.sturua@jina.ai>
2023-04-05 15:02:53 +0200

    feat: persist index
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- datastore/executor/docarray_v1.py
+++ datastore/executor/docarray_v1.py
@@ -5,13 +5,15 @@ from jina import Executor, requests, DocumentArray
 import asyncio
 
 
-DEFAULT_INDEX_PATH = os.environ.get("DOCARRAY_FILE_PATH", "./retrieval_da.bin")
-
-
 class DocArrayDataStore(Executor):
     def __init__(self, **kwargs):
         super().__init__()
-        self._index_file_path = kwargs.get('index_file_path', DEFAULT_INDEX_PATH)
+        self._index_file_path = (
+            os.path.join(self.workspace, "retrieval_da.bin")
+            if self.workspace
+            else "retrieval_da.bin"
+        )
+        print(f"Index path set to {self._index_file_path}")
         try:
             self._index = DocumentArray.load_binary(self._index_file_path)
             print(f"Instantiated index with {len(self._index)} existing documents")
@@ -22,7 +24,9 @@ class DocArrayDataStore(Executor):
     @requests(on="/upsert")
     async def upsert(self, docs: DocumentArray, **kwargs) -> DocumentArray:
         # Delete any existing vectors for documents with the input document ids
-        await asyncio.gather(self.delete(docs=docs, parameters={}))
+        existing_docs = DocumentArray([doc for doc in docs if doc.id in self._index])
+        if existing_docs:
+            await asyncio.gather(self.delete(docs=existing_docs, parameters={}))
 
         docs_to_append = docs[...]
         self._index.extend(docs[...])
@@ -32,18 +36,21 @@ class DocArrayDataStore(Executor):
     @requests(on="/query")
     async def query(self, docs: DocumentArray, **kwargs) -> DocumentArray:
         result_docs = DocumentArray()
-        for doc in docs:  # TODO(johannes) this can probably be rewritten without the loop
-            filter_query = self._get_query_from_filters(doc.tags.get('filters'))
+        for (
+            doc
+        ) in docs:  # TODO(johannes) this can probably be rewritten without the loop
+            filter_query = self._get_query_from_filters(doc.tags.get("filters"))
             docs_to_search = self._index
             if filter_query:
                 docs_to_search = self._index.find(filter_query)
             matches = docs_to_search.find(doc.embedding, top_k=doc.tags["top_k"])
             result_docs.append(DADoc(id=doc.id, chunks=matches))
-
         return result_docs
 
     @requests(on="/delete")
-    async def delete(self, docs: DocumentArray, parameters: Dict, **kwargs) -> DocumentArray:
+    async def delete(
+        self, docs: DocumentArray, parameters: Dict, **kwargs
+    ) -> DocumentArray:
         delete_all = parameters.get("delete_all", False)
         ids = docs[:, "id"]
         filters = parameters.get("filters", None)
@@ -67,4 +74,4 @@ class DocArrayDataStore(Executor):
     def _get_query_from_filters(filters: Dict[str, Any]) -> Dict:
         if not filters:
             return {}
-        return {'$and': [{filter: {'$eq': value}} for filter, value in filters.items()]}
+        return {"$and": [{filter: {"$eq": value}} for filter, value in filters.items()]}
