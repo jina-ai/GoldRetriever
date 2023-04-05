1ae938de6d7b52c89f548640fefe84f513e3afc2
jupyterjazz <saba.sturua@jina.ai>
2023-04-05 15:40:57 +0200

    refactor: modify cli for deploy
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- retrieval-plugin.py
+++ retrieval-plugin.py
@@ -1,13 +1,39 @@
 import os
+import random
+import string
+import tempfile
 from argparse import Namespace
+from pathlib import Path
 from typing import Optional
-from jina import Flow
-from gateway import RetrievalGateway
-from datastore.executor.docarray_v1 import DocArrayDataStore
 
+import jwt
 import typer
+from hubble.api import login as login_jina
+from jcloud.api import deploy as deploy_flow
+from jina import Flow
+
+from datastore.executor.docarray_v1 import DocArrayDataStore
+from gateway import RetrievalGateway
 
 app = typer.Typer()
+
+
+def random_string(size=16):
+    return "".join(
+        random.SystemRandom().choice(string.ascii_uppercase + string.digits)
+        for _ in range(size)
+    )
+
+
+def random_number(size=16):
+    return int(
+        "".join(random.SystemRandom().choice(string.digits) for _ in range(size))
+    )
+
+
+def generate_bearer_token():
+    payload = {"sub": random_string(), "iat": random_number()}
+    return jwt.encode(payload, "my_secret", algorithm="HS256")
 
 
 @app.command()
@@ -48,30 +74,47 @@ def launch(bearer_token: Optional[str], openai_token: Optional[str]):
             .add(uses="docker://gpt-plugin-indexer")
         )
     except Exception as e:
-        print('Did you build the docker images? You can also use the `launch_no_docker` command to run without docker.')
+        print(
+            "Did you build the docker images? You can also use the `launch_no_docker` command to run without docker."
+        )
         raise e
     with flow:
         flow.block()
 
 
 @app.command()
-def deploy(bearer_token: Optional[str], openai_token: Optional[str]):
-    from pathlib import Path
-    from jcloud.api import deploy as deploy_flow
-    import tempfile
-    from hubble.api import login as login_jina
-
+def deploy(
+    bearer_token: Optional[str] = typer.Option(None),
+    openai_token: Optional[str] = typer.Option(None),
+):
     args = Namespace(force=False)
     login_jina(args)
 
-    config_str = Path('flow.yml').read_text()
-    config_str = config_str.replace('<your-openai-api-key>', openai_token)
-    config_str = config_str.replace('<your-bearer-token>', bearer_token)
+    bearer_token = bearer_token or os.environ.get("RETRIEVAL_BEARER_TOKEN")
+    if not bearer_token:
+        bearer_token = generate_bearer_token()
+        os.environ["RETRIEVAL_BEARER_TOKEN"] = bearer_token
+
+    openai_token = openai_token or os.environ.get("RETRIEVAL_OPENAI_TOKEN")
+    if not openai_token:
+        raise ValueError(
+            "No OpenAI key is provided. You should either export your "
+            "key as an environment variable `RETRIEVAL_OPENAI_TOKEN` or "
+            "provide it through the CLI `--openai-token <your token>`"
+        )
+
+    config_str = Path("flow.yml").read_text()
+    config_str = config_str.replace("<your-openai-api-key>", openai_token)
+    config_str = config_str.replace("<your-bearer-token>", bearer_token)
     tmp_config_file, tmp_config_path = tempfile.mkstemp()
     try:
-        with os.fdopen(tmp_config_file, 'w') as tmp:
+        with os.fdopen(tmp_config_file, "w") as tmp:
             tmp.write(config_str)
             args = Namespace(path=tmp_config_path)
+        print(
+            f"Your Bearer token for this deployment is - {bearer_token} - "
+            f"Please store it as you will need it to interact with the plugin."
+        )
         deploy_flow(args)
 
     finally:
