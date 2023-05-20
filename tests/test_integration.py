4f65d5eb91f8df2fcd71d925955c3392df6a576f
jupyterjazz <saba.sturua@jina.ai>
2023-05-20 23:49:53 +0200

    test: go sleep bro
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 
 from typer.testing import CliRunner
 from goldretriever.retriever import app, get_flows
+import time
 
 class TestRetriever(unittest.TestCase):
     def setUp(self):
@@ -15,7 +16,7 @@ class TestRetriever(unittest.TestCase):
     def test_create_plugin(self):
         result = self.runner.invoke(app, ['deploy', '--key', self.key])
         self.assertIn('Flow is available!', result.stdout.strip())
-
+        time.sleep(5)
         flows = get_flows(keyword='test-retrieval')
         self.assertEqual(len(flows), 1)
 
