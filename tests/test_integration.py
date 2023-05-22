585b866571a0426ebcac04c64e50db7d003da538
Saba Sturua <45267439+jupyterjazz@users.noreply.github.com>
2023-05-22 11:14:08 +0200

    fix: jcloud survey (#23)
    * fix: jcloud survey
    
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>
    
    * chore: add bearer token
    
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>
    
    * chore: revert bearer changes
    
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>
    
    * chore: revert cd
    
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>
    
    ---------
    
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,12 +11,13 @@ class TestRetriever(unittest.TestCase):
         self.runner = CliRunner()
         self.key = os.environ['RETRIEVAL_OPENAI_KEY']
         self.test_flow_path = 'tests/resources/test_flow.yml'
+        os.environ['JCLOUD_NO_SURVEY'] = 'no_survey_please'
 
     @patch('goldretriever.retriever.FLOW_PATH', 'tests/resources/test_flow.yml')
     def test_create_plugin(self):
         result = self.runner.invoke(app, ['deploy', '--key', self.key])
         self.assertIn('Flow is available!', result.stdout.strip())
-        time.sleep(15)
+        time.sleep(5)
         flows = get_flows(keyword='test-retrieval')
         self.assertEqual(len(flows), 1)
 
