ef2d74403f223662ebfb251a356c778079f44616
jupyterjazz <saba.sturua@jina.ai>
2023-05-22 10:04:28 +0200

    fix: jcloud survey
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -11,10 +11,12 @@ class TestRetriever(unittest.TestCase):
         self.runner = CliRunner()
         self.key = os.environ['RETRIEVAL_OPENAI_KEY']
         self.test_flow_path = 'tests/resources/test_flow.yml'
+        os.environ['JCLOUD_NO_SURVEY'] = 'no_survey_please'
 
     @patch('goldretriever.retriever.FLOW_PATH', 'tests/resources/test_flow.yml')
     def test_create_plugin(self):
         result = self.runner.invoke(app, ['deploy', '--key', self.key])
+        print('deploy result', result.stdout.strip())
         self.assertIn('Flow is available!', result.stdout.strip())
         time.sleep(15)
         flows = get_flows(keyword='test-retrieval')
@@ -24,11 +26,16 @@ class TestRetriever(unittest.TestCase):
         self.assertEqual(os.environ['RETRIEVAL_FLOW_ID'], flow_id)
 
         self.runner.invoke(app, ['index', '--data', 'tests/resources/text_data/'])
+        print('index result', result.stdout.strip())
 
         result = self.runner.invoke(app, ['query', 'blue'])
+        print('query result', result.stdout.strip())
+
         self.assertIn('Blue is a primary color', result.stdout.strip())
 
         result = self.runner.invoke(app, ['delete', flow_id])
+        print('delete result', result.stdout.strip())
+
         self.assertIn('was successfully deleted', result.stdout.strip())
 
 if __name__ == '__main__':
