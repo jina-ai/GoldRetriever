c29f7ccc87906c6db821cc52bb31af24e21b73fd
jupyterjazz <saba.sturua@jina.ai>
2023-05-22 11:03:23 +0200

    chore: revert bearer changes
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,29 +12,28 @@ class TestRetriever(unittest.TestCase):
         self.key = os.environ['RETRIEVAL_OPENAI_KEY']
         self.test_flow_path = 'tests/resources/test_flow.yml'
         os.environ['JCLOUD_NO_SURVEY'] = 'no_survey_please'
-        self.bearer_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFTkU3RjU1NTVFVjg4R08wIiwiaWF0Ijo3MjUxODgxNjQ5NTE4OTMyfQ.SH0zQ5vcau1ZHu3QrH2z38QtI_Y865nBXfn8zIZOKk8'
 
     @patch('goldretriever.retriever.FLOW_PATH', 'tests/resources/test_flow.yml')
     def test_create_plugin(self):
-        result = self.runner.invoke(app, ['deploy', '--key', self.key, '--bearer-token', self.bearer_token])
+        result = self.runner.invoke(app, ['deploy', '--key', self.key])
         print('deploy result', result.stdout.strip())
         self.assertIn('Flow is available!', result.stdout.strip())
-        time.sleep(15)
+        time.sleep(5)
         flows = get_flows(keyword='test-retrieval')
         self.assertEqual(len(flows), 1)
 
         flow_id = flows[0]['id']
         self.assertEqual(os.environ['RETRIEVAL_FLOW_ID'], flow_id)
 
-        self.runner.invoke(app, ['index', '--data', 'tests/resources/text_data/', '--bearer-token', self.bearer_token])
+        self.runner.invoke(app, ['index', '--data', 'tests/resources/text_data/'])
         print('index result', result.stdout.strip())
 
-        result = self.runner.invoke(app, ['query', 'blue', '--bearer-token', self.bearer_token])
+        result = self.runner.invoke(app, ['query', 'blue'])
         print('query result', result.stdout.strip())
 
         self.assertIn('Blue is a primary color', result.stdout.strip())
 
-        result = self.runner.invoke(app, ['delete', flow_id, '--bearer-token', self.bearer_token])
+        result = self.runner.invoke(app, ['delete', flow_id])
         print('delete result', result.stdout.strip())
 
         self.assertIn('was successfully deleted', result.stdout.strip())
