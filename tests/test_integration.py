30aafb246c6ca67ec8599939121dbdf66e9e425d
jupyterjazz <saba.sturua@jina.ai>
2023-05-22 10:47:53 +0200

    chore: add bearer token
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,10 +12,11 @@ class TestRetriever(unittest.TestCase):
         self.key = os.environ['RETRIEVAL_OPENAI_KEY']
         self.test_flow_path = 'tests/resources/test_flow.yml'
         os.environ['JCLOUD_NO_SURVEY'] = 'no_survey_please'
+        self.bearer_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFTkU3RjU1NTVFVjg4R08wIiwiaWF0Ijo3MjUxODgxNjQ5NTE4OTMyfQ.SH0zQ5vcau1ZHu3QrH2z38QtI_Y865nBXfn8zIZOKk8'
 
     @patch('goldretriever.retriever.FLOW_PATH', 'tests/resources/test_flow.yml')
     def test_create_plugin(self):
-        result = self.runner.invoke(app, ['deploy', '--key', self.key])
+        result = self.runner.invoke(app, ['deploy', '--key', self.key, '--bearer-token', self.bearer_token])
         print('deploy result', result.stdout.strip())
         self.assertIn('Flow is available!', result.stdout.strip())
         time.sleep(15)
@@ -25,15 +26,15 @@ class TestRetriever(unittest.TestCase):
         flow_id = flows[0]['id']
         self.assertEqual(os.environ['RETRIEVAL_FLOW_ID'], flow_id)
 
-        self.runner.invoke(app, ['index', '--data', 'tests/resources/text_data/'])
+        self.runner.invoke(app, ['index', '--data', 'tests/resources/text_data/', '--bearer-token', self.bearer_token])
         print('index result', result.stdout.strip())
 
-        result = self.runner.invoke(app, ['query', 'blue'])
+        result = self.runner.invoke(app, ['query', 'blue', '--bearer-token', self.bearer_token])
         print('query result', result.stdout.strip())
 
         self.assertIn('Blue is a primary color', result.stdout.strip())
 
-        result = self.runner.invoke(app, ['delete', flow_id])
+        result = self.runner.invoke(app, ['delete', flow_id, '--bearer-token', self.bearer_token])
         print('delete result', result.stdout.strip())
 
         self.assertIn('was successfully deleted', result.stdout.strip())
